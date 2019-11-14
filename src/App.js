e384fb2e9ec7137f26a19404e89c05ae570fcfa0
je-moore <jacksonmoore@tuta.io>
2019-11-14 20:06:26 +0100

    implement grid, update rules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,137 @@
-import React from 'react'
+import React, { useState, useCallback, useRef } from 'react'
+import produce from 'immer'
 
-function App() {
-  return <div className="App"></div>
+const numRows = 100
+const numCols = 162
+const cellSize = 100 / numCols + 'vw'
+const rate = 10
+const density = 0.7
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+]
+
+const generateEmptyGrid = () => {
+  const rows = []
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0))
+  }
+
+  return rows
+}
+
+const generateRandomGrid = () => {
+  const rows = []
+  for (let i = 0; i < numRows; i++) {
+    rows.push(
+      Array.from(Array(numCols), () => (Math.random() > density ? 1 : 0))
+    )
+  }
+
+  return rows
+}
+
+const App = () => {
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGrid()
+  })
+
+  const [running, setRunning] = useState(false)
+
+  const runningRef = useRef(running)
+  runningRef.current = running
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return
+    }
+
+    setGrid(g => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0
+            operations.forEach(([x, y]) => {
+              const newI = i + x
+              const newK = k + y
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK]
+              }
+            })
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1
+            }
+          }
+        }
+      })
+    })
+    setTimeout(runSimulation, rate)
+  }, [])
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setRunning(!running)
+          if (!running) {
+            runningRef.current = true
+            runSimulation()
+          }
+        }}
+      >
+        {running ? 'stop' : 'start'}
+      </button>
+      <button
+        onClick={() => {
+          setGrid(generateRandomGrid())
+        }}
+      >
+        random
+      </button>
+      <button
+        onClick={() => {
+          setGrid(generateEmptyGrid())
+        }}
+      >
+        clear
+      </button>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${numCols}, ${cellSize})`,
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1
+                })
+                setGrid(newGrid)
+              }}
+              style={{
+                width: cellSize,
+                height: cellSize,
+                backgroundColor: grid[i][k] ? 'black' : 'white',
+              }}
+            />
+          ))
+        )}
+      </div>
+    </>
+  )
 }
 
 export default App
