7cc3344060f1e684f76163241a6f8798fe740628
je-moore <jacksonmoore@tuta.io>
2019-11-15 16:57:40 +0100

    flatten grid array

--- src/App.js
+++ src/App.js
@@ -1,61 +1,43 @@
 import React, { useState, useCallback, useRef } from 'react'
 import produce from 'immer'
 
-const numRows = 100
-const numCols = 162
+const numRows = 124
+const numCols = 200
 const cellSize = 100 / numCols + 'vw'
 const rate = 1
 const density = 0.7
 
-const operations = [
-  [0, 1],
-  [0, -1],
-  [1, -1],
-  [-1, 1],
-  [1, 1],
-  [-1, -1],
-  [1, 0],
-  [-1, 0],
+const neighborOffsets = [
+  -1,
+  1,
+  numCols - 1,
+  numCols,
+  numCols + 1,
+  -numCols - 1,
+  -numCols,
+  -numCols + 1,
 ]
 
-const generateEmptyGrid = () => {
-  const rows = []
-  for (let i = 0; i < numRows; i++) {
-    rows.push(Array(numCols).fill(0))
-  }
+const generateEmptyGrid = () => Array(numCols * numRows).fill(0)
 
-  return rows
-}
-
-const generateRandomGrid = () => {
-  const rows = []
-  for (let i = 0; i < numRows; i++) {
-    rows.push(
-      Array.from(Array(numCols), () => (Math.random() > density ? 1 : 0))
-    )
-  }
-
-  return rows
-}
+const generateRandomGrid = () =>
+  Array.from(Array(numCols * numRows), () => (Math.random() > density ? 1 : 0))
 
 const updateGrid = grid => {
   let newGrid = generateEmptyGrid()
-  for (let i = 0; i < numRows; i++) {
-    for (let k = 0; k < numCols; k++) {
-      let neighbors = 0
-      operations.forEach(([x, y]) => {
-        const newI = i + x
-        const newK = k + y
-        if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
-          neighbors += grid[newI][newK]
-        }
-      })
-
-      if (grid[i][k] === 1 && (neighbors === 3 || neighbors === 2)) {
-        newGrid[i][k] = 1
-      } else if (grid[i][k] === 0 && neighbors === 3) {
-        newGrid[i][k] = 1
+  for (let i = 0; i < numRows * numCols; i++) {
+    let neighbors = 0
+    neighborOffsets.forEach(x => {
+      const neighborIndex = i + x
+      if (neighborIndex >= 0 && neighborIndex < numRows * numCols) {
+        neighbors += grid[neighborIndex]
       }
+    })
+
+    if (grid[i] === 1 && (neighbors === 3 || neighbors === 2)) {
+      newGrid[i] = 1
+    } else if (grid[i] === 0 && neighbors === 3) {
+      newGrid[i] = 1
     }
   }
   return newGrid
@@ -113,24 +95,22 @@ const App = () => {
           gridTemplateColumns: `repeat(${numCols}, ${cellSize})`,
         }}
       >
-        {grid.map((rows, i) =>
-          rows.map((col, k) => (
-            <div
-              key={`${i}-${k}`}
-              onClick={() => {
-                const newGrid = produce(grid, gridCopy => {
-                  gridCopy[i][k] = grid[i][k] ? 0 : 1
-                })
-                setGrid(newGrid)
-              }}
-              style={{
-                width: cellSize,
-                height: cellSize,
-                backgroundColor: grid[i][k] ? 'black' : 'white',
-              }}
-            />
-          ))
-        )}
+        {grid.map((rows, i) => (
+          <div
+            key={i}
+            onClick={() => {
+              const newGrid = produce(grid, gridCopy => {
+                gridCopy[i] = grid[i] ? 0 : 1
+              })
+              setGrid(newGrid)
+            }}
+            style={{
+              width: cellSize,
+              height: cellSize,
+              backgroundColor: grid[i] ? 'black' : 'white',
+            }}
+          />
+        ))}
       </div>
     </>
   )
